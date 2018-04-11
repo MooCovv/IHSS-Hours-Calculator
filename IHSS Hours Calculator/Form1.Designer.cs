8638377d48a10b17b622aa9ac89d3847beedfe01
PHILIP ROBINSON <philip.robinson@lausd.net>
2018-04-10 17:18:18 -0700

    Fixed bug: hours and days where swapped

--- IHSS Hours Calculator/Form1.Designer.cs
+++ IHSS Hours Calculator/Form1.Designer.cs
@@ -28,13 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.daysTextBox = new System.Windows.Forms.TextBox();
+            this.hoursTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.sevenHoursTextBox = new System.Windows.Forms.TextBox();
+            this.eightHoursTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of Days:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of Hours:";
+            // 
+            // daysTextBox
+            // 
+            this.daysTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.daysTextBox.Location = new System.Drawing.Point(152, 12);
+            this.daysTextBox.Name = "daysTextBox";
+            this.daysTextBox.Size = new System.Drawing.Size(100, 26);
+            this.daysTextBox.TabIndex = 2;
+            // 
+            // hoursTextBox
+            // 
+            this.hoursTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hoursTextBox.Location = new System.Drawing.Point(152, 67);
+            this.hoursTextBox.Name = "hoursTextBox";
+            this.hoursTextBox.Size = new System.Drawing.Size(100, 26);
+            this.hoursTextBox.TabIndex = 3;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(76, 116);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(120, 42);
+            this.calculateButton.TabIndex = 4;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(76, 303);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(120, 42);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // sevenHoursTextBox
+            // 
+            this.sevenHoursTextBox.Enabled = false;
+            this.sevenHoursTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sevenHoursTextBox.Location = new System.Drawing.Point(23, 183);
+            this.sevenHoursTextBox.Name = "sevenHoursTextBox";
+            this.sevenHoursTextBox.Size = new System.Drawing.Size(229, 26);
+            this.sevenHoursTextBox.TabIndex = 6;
+            // 
+            // eightHoursTextBox
+            // 
+            this.eightHoursTextBox.Enabled = false;
+            this.eightHoursTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eightHoursTextBox.Location = new System.Drawing.Point(23, 233);
+            this.eightHoursTextBox.Name = "eightHoursTextBox";
+            this.eightHoursTextBox.Size = new System.Drawing.Size(229, 26);
+            this.eightHoursTextBox.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(220, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Add the remaining muntes to one of the days.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(279, 366);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.eightHoursTextBox);
+            this.Controls.Add(this.sevenHoursTextBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.hoursTextBox);
+            this.Controls.Add(this.daysTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "IHSS Hours Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox daysTextBox;
+        private System.Windows.Forms.TextBox hoursTextBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.TextBox sevenHoursTextBox;
+        private System.Windows.Forms.TextBox eightHoursTextBox;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- IHSS Hours Calculator/Form1.cs
+++ IHSS Hours Calculator/Form1.cs
@@ -16,5 +16,37 @@ namespace IHSS_Hours_Calculator
         {
             InitializeComponent();
         }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numHours = Convert.ToInt32(hoursTextBox.Text);
+                int numDays = Convert.ToInt32(daysTextBox.Text);
+
+                if (numHours > 200)
+                {
+                    numHours = numHours / 2;
+                }
+
+                int numOfHours1 = numHours / numDays;
+                int numCheck = numOfHours1 * numDays;
+                int numOfDays2 = numHours - numCheck;
+                int numOfDays1 = numDays - numOfDays2;
+                int numOfHours2 = numOfHours1 + 1;
+
+                sevenHoursTextBox.Text = numOfDays1.ToString() + " days with " + numOfHours1.ToString() + " hours each.";
+                eightHoursTextBox.Text = numOfDays2.ToString() + " days with " + numOfHours2.ToString() + " hours each.";
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
